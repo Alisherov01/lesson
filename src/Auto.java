48a5c1420b5af3a726f5d5e74b13701515056220
erlan_alisherov <alisherovv7@bk.ru>
2022-11-08 19:47:03 +0600

    new commit

---
+++ src/Auto.java
@@ -0,0 +1,14 @@
+public class Auto {
+    String name;
+
+    public Auto(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto myAuto = new Auto("Toyota");
+        System.out.println(myAuto);
+    }
+}
