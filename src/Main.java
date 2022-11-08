f95e958f3ddf584b9315f7925123bcfc22f22455
erlan_alisherov <alisherovv7@bk.ru>
2022-11-08 20:07:18 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Auto myAuto = new Auto("Toyota");
         System.out.println(myAuto);
+        System.out.println("Эта новая ветка с моим именем");
     }
 }
